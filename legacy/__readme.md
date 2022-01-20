936c998b7873ad04e81468fec465c23aec940722
paulg <12997377+PaulGarnerUk@users.noreply.github.com>
2022-01-20 21:55:45 +0000

    Moved a lot of legacy pages out of the root folder. The relocation will break the pages, but I might fix them as/when they become useful for comparing legacy pages to the new pages. Longer term I expect they can be deleted.  Small updates to the schedule page now that meets are being scheduled.  This page still needs many updates to pull the data from sql - but not today.

---
+++ legacy/__readme.md
@@ -0,0 +1,1 @@
+These are considered legacy pages which will not be maintained. It might be worth them hanging around for a while for a/b comparisons with the new pages and schema.
---
+++ schedule-old.php
@@ -0,0 +1,570 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<meta name="description" content="Schedule of league meets for the 2022 season of the UK Pinball League" />
+<title>UK Pinball League – 2019 Schedule</title>
+<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<?php include("favicons.php"); ?>
+
+<link rel="stylesheet" type="text/css" href="flexdropdown.css" />
+<link rel="stylesheet" type="text/css" href="ukpbl.css" />
+
+<!-- SlickNav -->
+<link rel="stylesheet" href="slicknav.css">
+<script src="https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.6.2/modernizr.min.js"></script>
+
+<!-- Flex dropdown -->
+<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js"></script>
+<script type="text/javascript" src="flexdropdown.js">
+/***********************************************
+* Flex Level Drop Down Menu- (c) Dynamic Drive DHTML code library (www.dynamicdrive.com)
+* This notice MUST stay intact for legal use
+* Visit Dynamic Drive at http://www.dynamicdrive.com/ for this script and 100s more
+***********************************************/
+</script>
+
+
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-19413802-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+
+</head>
+
+<body>
+
+<!-- Background -->	
+<div id="background">
+
+<!-- Container -->
+<div id="container">
+	
+<!-- Banner -->
+<div id="banner"><img src="images/ukpl-banner.png" class="banner" width="107px">
+
+<!-- Navigation -->
+<div id="nav-wrapper">
+<?php include("responsive-menu.inc"); ?>
+</div>
+</div>
+
+<div id="main-menu">
+
+<p class="menu-text">
+<a href="index.php" class="menu">HOME</a> 
+<a href="schedule.php" class="menu-current">SCHEDULE</a> 
+<a href="#" class="menu" data-flexmenu="league-results">RESULTS</a>
+<a href="rankings.php" class="menu">RANKINGS</a>
+<a href="#" class="menu" data-flexmenu="scores">HI SCORES</a>
+ 
+<a href="#" class="menu" data-flexmenu="rules-faqs">RULES/FAQ</a> 
+<a href="links.php" class="menu">LINKS</a> 
+<a href="contacts.php" class="menu">CONTACTS</a>
+</p>
+
+<!--HTML for League Results-->
+<ul id="league-results" class="flexdropdownmenu">
+<li><a href="leaguetable-sw.php">SOUTH WEST</a></li>
+<li><a href="leaguetable-m.php">MIDLANDS</a></li>
+<li><a href="leaguetable-se.php">LONDON &amp; SE</a></li>
+<li><a href="leaguetable-n.php">NORTHERN</a></li>
+<li><a href="leaguetable-sc.php">SCOTTISH</a></li>
+<li><a href="leaguetable-ir.php">IRISH</a></li>
+<li><a href="finals.php">FINALS</a></li>
+<li><a href="placings.php">MEET WINNERS</a></li>
+<li><a href="winners.php">LEAGUE WINNERS</a></li>
+</ul>
+
+<!--HTML for Scores-->
+<ul id="scores" class="flexdropdownmenu">
+<li><a href="highaveragescores.php">HI / AVG SCORES</a></li>
+<li><a href="highscores10.php">HI SCORES 10</a></li>
+<li><a href="highscoresperplayer.php">HI PER PLAYER</a></li>
+</ul>
+
+<!--HTML for Rules/FAQs-->
+<ul id="rules-faqs" class="flexdropdownmenu">
+<li><a href="rules.php">RULES</a></li>
+<li><a href="faq.php">FAQs</a></li>
+</ul>
+
+</div><!--END MENU DIV-->
+
+<div id="menu-strip"></div>
+
+<div class="panel">
+
+<h1>UKPL Schedule – Season 15 (2021)</h1>
+
+<p class="firstline">The UK Pinball League season period is based on a calendar year.  Each region usually consists of up to six meets. Meets for the 15th season are currently being arranged and will be posted here as they are confirmed.</p>
+
+<p>For a full calendar of other pinball events see the excellent <a href="http://www.pinballnews.com/diary/index.html" class="link" target="_blank">Show Diary</a> page on the <a href="http://www.pinballnews.com" class="link" target="_blank">Pinball News</a> website.</p>
+
+</div>
+
+<div class="panel">
+	
+<h2>South West Region</h2>
+
+<div class="table-holder">
+<table class="responsive">
+  <thead>
+      <tr class="white">
+        <th scope="col" width="150px">Date</th>
+        <th scope="col" width="100px">Practice</th>
+        <th scope="col" width="100px">Competition</th>
+        <th scope="col" width="230px">Location</th>
+        <th scope="col" width="170px">Host</th>
+      </tr>
+    </thead>
+
+    <tbody>
+      
+      <tr>
+        <td>TBC</td>
+        <td>TBC</td>
+        <td>TBC</td>
+        <td>TBC</td>
+        <td class="paddidge">TBC</td>
+      </tr>
+
+      <tr>
+        <td>TBC</td>
+        <td>TBC</td>
+        <td>TBC</td>
+        <td>TBC</td>
+        <td class="paddidge">TBC</td>
+      </tr>
+
+      <tr>
+        <td>TBC</td>
+        <td>TBC</td>
+        <td>TBC</td>
+        <td>TBC</td>
+        <td class="paddidge">TBC</td>
+      </tr>
+
+      <tr>
+        <td>TBC</td>
+        <td>TBC</td>
+        <td>TBC</td>
+        <td>TBC</td>
+        <td class="paddidge">TBC</td>
+      </tr>
+
+      <tr>
+        <td>TBC</td>
+        <td>TBC</td>
+        <td>TBC</td>
+        <td>TBC</td>
+        <td class="paddidge">TBC</td>
+      </tr>
+
+      <tr>
+        <td>TBC</td>
+        <td>TBC</td>
+        <td>TBC</td>
+        <td>TBC</td>
+        <td class="paddidge">TBC</td>
+      </tr>
+                            
+    </tbody>
+        
+</table>
+</div>
+
+</div>
+
+<div class="panel">
+
+<h2>Midlands Region</h2>
+
+<div class="table-holder">
+<table class="responsive">
+	<thead>
+			<tr class="white">
+				<th scope="col" width="150px">Date</th>
+        <th scope="col" width="100px">Practice</th>
+        <th scope="col" width="100px">Competition</th>
+        <th scope="col" width="230px">Location</th>
+        <th scope="col" width="170px">Host</th>
+			</tr>
+		</thead>
+
+		<tbody>
+		  
+		  <tr>
+				<td>30.01.2022</td>
+        <td>12:00–14:00</td>
+        <td>14:00–16:00</td>
+        <td>Electric Circus, Fife St, Nuneaton</td>
+        <td class="paddidge">Steve Pagett</td>
+		  </tr>
+
+      <tr>
+        <td>27.02.2022</td>
+        <td>12:00–14:00</td>
+        <td>14:00–16:00</td>
+        <td>Grantham</td>
+        <td class="paddidge">John Bateson</td>
+      </tr>
+
+      <tr>
+        <td>20.03.2022</td>
+        <td>12:00–14:00</td>
+        <td>14:00–16:00</td>
+        <td>Northampton</td>
+        <td class="paddidge">Dan Lewell</td>
+      </tr>
+
+      <tr>
+        <td>10.04.2022</td>
+        <td>12:00–14:00</td>
+        <td>14:00–16:00</td>
+        <td>Electric Circus, Fife St, Nuneaton</td>
+        <td class="paddidge">Steve Pagett</td>
+      </tr>
+
+      <tr>
+        <td>08.05.2022</td>
+        <td>12:00–14:00</td>
+        <td>14:00–16:00</td>
+        <td>Newark</td>
+        <td class="paddidge">David Dodds</td>
+      </tr>
+
+      <tr>
+        <td>05.06.2022</td>
+        <td>12:00–14:00</td>
+        <td>14:00–16:00</td>
+        <td>Shepshed</td>
+        <td class="paddidge">Terry Sullivan</td>
+      </tr>
+		  		  								
+    </tbody>
+        
+</table>
+</div>
+
+</div>
+
+<div class="panel">
+
+<h2>London &amp; South East Region</h2>
+
+<div class="table-holder">
+<table class="responsive">
+	<thead>
+			<tr class="white">
+				<th scope="col" width="150px">Date</th>
+                 <th scope="col" width="100px">Practice</th>
+                <th scope="col" width="100px">Competition</th>
+                <th scope="col" width="230px">Location</th>
+                <th scope="col" width="170px">Host</th>
+           </tr>
+		</thead>
+
+		<tbody>
+
+      <tr>
+        <td>TBC</td>
+        <td>TBC</td>
+        <td>TBC</td>
+        <td>TBC</td>
+        <td class="paddidge">TBC</td>
+      </tr>
+
+      <tr>
+        <td>TBC</td>
+        <td>TBC</td>
+        <td>TBC</td>
+        <td>TBC</td>
+        <td class="paddidge">TBC</td>
+      </tr>
+
+      <tr>
+        <td>TBC</td>
+        <td>TBC</td>
+        <td>TBC</td>
+        <td>TBC</td>
+        <td class="paddidge">TBC</td>
+      </tr>
+
+      <tr>
+        <td>TBC</td>
+        <td>TBC</td>
+        <td>TBC</td>
+        <td>TBC</td>
+        <td class="paddidge">TBC</td>
+      </tr>
+
+      <tr>
+        <td>TBC</td>
+        <td>TBC</td>
+        <td>TBC</td>
+        <td>TBC</td>
+        <td class="paddidge">TBC</td>
+      </tr>
+
+      <tr>
+        <td>TBC</td>
+        <td>TBC</td>
+        <td>TBC</td>
+        <td>TBC</td>
+        <td class="paddidge">TBC</td>
+      </tr>
+
+    </tbody>
+        
+</table>
+</div>
+
+</div>
+
+<div class="panel">
+
+<h2>Northern Region</h2>
+
+<div class="table-holder">
+<table class="responsive">
+	<thead>
+			<tr class="white">
+				<th scope="col" width="150px">Date</th>
+                 <th scope="col" width="100px">Practice</th>
+                <th scope="col" width="100px">Competition</th>
+                <th scope="col" width="230px">Location</th>
+                <th scope="col" width="170px">Host</th>
+			</tr>
+		</thead>
+
+		<tbody>
+
+      <tr>
+				<td>28.11.2021</td>
+        <td>10:00–12:00</td>
+        <td>13:00–16:00</td>
+        <td>Marsden (Huddersfield)</td>
+        <td class="paddidge">David Dutton</td>
+		  </tr>
+
+      <tr class="strikeout">
+	<td>16.01.2022</td>
+        <td>Cancelled.</td>
+        <td>Cancelled</td>
+        <td>Arcade Hub, Hull</td>
+        <td class="paddidge">Arcade Hub</td>
+        </tr>
+
+      <tr>
+        <td>13.02.2022</td>
+        <td>10:00–12:00</td>
+        <td>13:00–16:00</td>
+        <td>Rawdon, Leeds</td>
+        <td class="paddidge">Alan Jones</td>
+      </tr>
+
+      <tr>
+        <td>27.03.2022</td>
+        <td>10:00–12:00</td>
+        <td>13:00–16:00</td>
+        <td>Preston Brook, Runcorn</td>
+        <td class="paddidge">Gary (Moose) Littler</td>
+      </tr>
+
+      <tr>
+        <td>24.04.2022</td>
+        <td>10:00–12:00</td>
+        <td>13:00–16:00</td>
+        <td>Southport</td>
+        <td class="paddidge">Paul Owen</td>
+      </tr>
+
+      <tr>
+        <td>15.05.2022</td>
+        <td>10:00–12:00</td>
+        <td>13:00–16:00</td>
+        <td>Southport</td>
+        <td class="paddidge">Andy Foster</td>
+      </tr>
+
+    </tbody>
+        
+</table>
+</div>
+
+</div>
+
+<div class="panel">
+
+<h2>Scottish Region</h2>
+
+<div class="table-holder">
+<table class="responsive">
+	<thead>
+			<tr class="white">
+				<th scope="col" width="150px">Date</th>
+        <th scope="col" width="100px">Practice</th>
+        <th scope="col" width="100px">Competition</th>
+        <th scope="col" width="230px">Location</th>
+        <th scope="col" width="170px">Host</th>
+			</tr>
+		</thead>
+
+		<tbody>
+
+      <tr>
+        <td>05.02.2022</td>
+        <td>11:00–13:00</td>
+        <td>13:30–18:00</td>
+        <td>Cumbernauld</td>
+        <td class="paddidge">Colin Clunie</td>
+      </tr>
+
+      <tr>
+        <td>March 2022 (TBC)</td>
+        <td>(TBC)</td>
+        <td>(TBC)</td>
+        <td>Edinburgh</td>
+        <td class="paddidge">Eric Ridley</td>
+      </tr>
+
+      <tr>
+        <td>16.04.2022</td>
+        <td>11:00–13:00</td>
+        <td>13:30–18:00</td>
+        <td>Glasgow</td>
+        <td class="paddidge">John Higgins</td>
+      </tr>
+
+      <tr>
+        <td>May/June 2022 (TBC)</td>
+        <td>(TBC)</td>
+        <td>(TBC)</td>
+        <td>Dundee</td>
+        <td class="paddidge">Chris Edgar</td>
+      </tr>
+
+    </tbody>
+        
+</table>
+</div>
+
+</div>
+
+
+<div class="panel">
+
+<h2>Irish Region</h2>
+
+<div class="table-holder">
+<table class="responsive">
+	<thead>
+			<tr class="white">
+				<th scope="col" width="150px">Date</th>
+        <th scope="col" width="100px">Practice</th>
+        <th scope="col" width="100px">Competition</th>
+        <th scope="col" width="230px">Location</th>
+        <th scope="col" width="170px">Host</th>
+			</tr>
+		</thead>
+
+		<tbody>
+
+      <tr>
+        <td>TBC</td>
+        <td>TBC</td>
+        <td>TBC</td>
+        <td>TBC</td>
+        <td class="paddidge">TBC</td>
+      </tr>
+
+      <tr>
+        <td>TBC</td>
+        <td>TBC</td>
+        <td>TBC</td>
+        <td>TBC</td>
+        <td class="paddidge">TBC</td>
+      </tr>
+
+      <tr>
+        <td>TBC</td>
+        <td>TBC</td>
+        <td>TBC</td>
+        <td>TBC</td>
+        <td class="paddidge">TBC</td>
+      </tr>
+
+      <tr>
+        <td>TBC</td>
+        <td>TBC</td>
+        <td>TBC</td>
+        <td>TBC</td>
+        <td class="paddidge">TBC</td>
+      </tr>
+
+      <tr>
+        <td>TBC</td>
+        <td>TBC</td>
+        <td>TBC</td>
+        <td>TBC</td>
+        <td class="paddidge">TBC</td>
+      </tr>
+
+      <tr>
+        <td>TBC</td>
+        <td>TBC</td>
+        <td>TBC</td>
+        <td>TBC</td>
+        <td class="paddidge">TBC</td>
+      </tr>
+
+    </tbody>
+        
+</table>
+</div>
+
+</div>
+
+<div class="panel-copyright">
+
+<!-- Footer -->
+<p class="copyright">&copy; UK Pinball League 2019</p>
+
+</div>
+
+<div class="panel-bottom"></div>
+
+</div> <!-- End Container -->
+
+<!-- SlickNav start -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
+<script src="jquery.slicknav.js"></script>
+<script type="text/javascript">
+$(document).ready(function(){
+	$('#menu').slicknav({
+	prependTo:'#nav-wrapper',
+    closeOnClick:'true' // Close menu when a link is clicked.	
+	});
+});
+</script>
+<!-- SlickNav end -->
+
+<!-- Responsive tables -->
+<link type="text/css" media="screen" rel="stylesheet" href="responsive-tables.css" />
+<script type="text/javascript" src="responsive-tables.js"></script>
+
+</body>
+  </html>
