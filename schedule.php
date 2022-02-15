93abeba034ba3e7c07e8c386aca25b33afc7a2fa
paulg <12997377+PaulGarnerUk@users.noreply.github.com>
2022-02-15 21:52:14 +0000

    I don't understand css media rules yet..

--- schedule.php
+++ schedule.php
@@ -3,6 +3,7 @@
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 <meta name="description" content="Schedule of league meets." />
+<meta name="viewport" content="width=device-width, initial-scale=1">
 <title>UK Pinball League - Schedule</title>
 <meta http-equiv="X-UA-Compatible" content="IE=edge" />
 
