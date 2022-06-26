284bf2c983a07ddf92ad49a9d217916485c6bf23
Paul G <12997377+PaulGarnerUk@users.noreply.github.com>
2022-06-26 17:22:07 +0100

    Standardise more of the header/footer sections.

--- links.php
+++ links.php
@@ -6,105 +6,8 @@
 <title>UK Pinball League – Links</title>
 <meta http-equiv="X-UA-Compatible" content="IE=edge" />
 
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-<?php include("favicons.php"); ?>
-
-<link rel="stylesheet" type="text/css" href="css/flexdropdown.css" />
-<link rel="stylesheet" type="text/css" href="css/ukpbl.css" />
-
-<!-- SlickNav stuff -->
-<link rel="stylesheet" href="css/slicknav.css">
-
-<script src="https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.6.2/modernizr.min.js"></script>
-
-<!-- SlickNav stuff end -->
-
-<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js"></script>
-<script type="text/javascript" src="js/flexdropdown.js">
-/***********************************************
-* Flex Level Drop Down Menu- (c) Dynamic Drive DHTML code library (www.dynamicdrive.com)
-* This notice MUST stay intact for legal use
-* Visit Dynamic Drive at http://www.dynamicdrive.com/ for this script and 100s more
-***********************************************/
-</script>
-
-<script type="text/javascript">
-
-  var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-19413802-1']);
-  _gaq.push(['_trackPageview']);
-
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
-
-</script>
-
-</head>
-
-<body>
-
-<!-- Background -->	
-<div id="background">
-
-<!-- Container -->
-<div id="container">
-	
-<!-- Banner -->
-<div id="banner"><img src="images/ukpl-banner.png" class="banner" width="107px">
-
-<!-- Navigation -->
-<div id="nav-wrapper">
-<?php include("includes/responsive-menu.inc"); ?>
-</div>
-</div>
-
-<div id="main-menu">
-
-<p class="menu-text">
-<a href="index.php" class="menu">HOME</a> 
-<a href="schedule.php" class="menu">SCHEDULE</a> 
-<a href="#" class="menu" data-flexmenu="league-results">RESULTS</a>
-<a href="rankings.php" class="menu">RANKINGS</a>
-<a href="#" class="menu" data-flexmenu="scores">HI SCORES</a>
- 
-<a href="#" class="menu" data-flexmenu="rules-faqs">RULES/FAQ</a> 
-<a href="links.php" class="menu">LINKS</a> 
-<a href="contacts.php" class="menu">CONTACTS</a>
-</p>
-
-<!--HTML for League Results-->
-<ul id="league-results" class="flexdropdownmenu">
-<li><a href="league.php?region=sw">SOUTH WEST</a></li>
-<li><a href="league.php?region=m">MIDLANDS</a></li>
-<li><a href="league.php?region=lse">LONDON &amp; SE</a></li>
-<li><a href="league.php?region=n">NORTHERN</a></li>
-<li><a href="league.php?region=s">SCOTTISH</a></li>
-<li><a href="league.php?region=i">IRISH</a></li>
-<li><a href="finals.php">FINALS</a></li>
-<li><a href="placings.php">MEET WINNERS</a></li>
-<li><a href="winners.php">LEAGUE WINNERS</a></li>
-</ul>
-
-<!--HTML for Scores-->
-<ul id="scores" class="flexdropdownmenu">
-<li><a href="highaveragescores.php">HI / AVG SCORES</a></li>
-<li><a href="highscores10.php">HI SCORES 10</a></li>
-<li><a href="highscoresperplayer.php">HI PER PLAYER</a></li>
-</ul>
-
-<!--HTML for Rules/FAQs-->
-<ul id="rules-faqs" class="flexdropdownmenu">
-<li><a href="rules.php">RULES</a></li>
-<li><a href="faq.php">FAQs</a></li>
-</ul>
-
-</div><!--END MENU DIV-->
-
-<div id="menu-strip"></div>
+<!-- Header and menu -->
+<?php include("includes/header.inc"); ?>
 
 <div class="panel">
 
@@ -156,29 +59,8 @@
 
 </div>
 
-<div class="panel-copyright">
-
 <!-- Footer -->
-<p class="copyright">&copy; UK Pinball League 2010-<?=date("Y");?></p>
-
-</div>
-
-<div class="panel-bottom"></div>
-
-</div> <!-- End Container -->
-
-<!-- SlickNav start -->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
-<script src="js/jquery.slicknav.js"></script>
-<script type="text/javascript">
-$(document).ready(function(){
-	$('#menu').slicknav({
-	prependTo:'#nav-wrapper',
-    closeOnClick:'true' // Close menu when a link is clicked.	
-	});
-});
-</script>
-<!-- SlickNav end -->
+<?php include("includes/footer.inc"); ?>
 
 </body>
-  </html>
+</html>
