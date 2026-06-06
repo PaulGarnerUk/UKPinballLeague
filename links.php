ba54fb982cb0d6a82e7b9be72ebd478c03a4ddbd
Paul G <12997377+PaulGarnerUk@users.noreply.github.com>
2026-06-06 17:59:13 +0100

    Force css reload

--- links.php
+++ links.php
@@ -10,7 +10,7 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Barlow+Condensed:wght@400;600;700&family=Inter:wght@400;500;600&display=swap" rel="stylesheet">
 
-    <link rel="stylesheet" href="css/modern.css">
+    <link rel="stylesheet" href="css/modern.css?v=1">
 </head>
 <body>
 
