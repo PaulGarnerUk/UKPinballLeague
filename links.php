a65801d8ecf8da7e405829f922626541e304bf45
Paul G <12997377+PaulGarnerUk@users.noreply.github.com>
2026-06-06 17:52:29 +0100

    Modernise and update the links page

--- links.php
+++ links.php
@@ -1,65 +1,156 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<!DOCTYPE html>
+<html lang="en">
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-<meta name="description" content="Links to useful and interesting pinball-related websites" />
-<title>UK Pinball League – Links</title>
-<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Links to useful and interesting pinball-related websites">
+    <title>UK Pinball League – Links</title>
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Barlow+Condensed:wght@400;600;700&family=Inter:wght@400;500;600&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="css/modern.css">
+</head>
+<body>
 
 <!-- Header and menu -->
-<?php include("includes/header.inc"); ?>
+<?php require_once('includes/header-modern.inc'); ?>
 
-<div class="panel">
-
-<h1>Useful Links</h1>
-
-<h2>Pinball information</h2>
-<p><a href="https://en-gb.facebook.com/UKPinballLeague/" class="link" target="_blank">Facebook</a> – The Facebook page for the UK Pinball League.</p>
-<p><a href="https://www.pinballinfo.com/community/" class="link" target="_blank">Pinball Info</a> – Forum for discussing all aspects of pinball.</p>
-<p><a href="https://www.pinballnews.com/site/" class="link" target="_blank">Pinball News</a> – Reviews of latest machines, national and international tournament reports, location reports and more.</p>
-<p><a href="https://www.ifpapinball.com/" class="link" target="_blank">Pinball Rankings</a> – Current national and world rankings of all registered pinball players.</p>
-<p><a href="https://www.ipdb.org/search.pl" class="link" target="_blank">Internet Pinball Database</a> – Database of every pinball machine, including prototypes, production run data, pictures, flyers and manuals.</p>
-<p><a href="https://www.homeleisuredirect.com/pinball-machines/" class="link" target="_blank">Home Leisure Direct</a> – 'How to Play Pinball' videos, buyers' guides and articles.</p>
-<p><a href="https://papa.org/learning-center/players-guide/?target=rule-sheets" class="link" target="_blank">Rulesheets</a> – Covers all the rules for a game and usually includes some tips, tricks and hidden features.</p>
-<p><a href="http://www.pinballnews.com/learn/acronyms.html" class="link" target="_blank">Acronyms</a> – A list of all commonly used acronyms for machines; eg AFM is 'Attack from Mars'.</p>
-<p><a href="http://www.pinformer.co.uk/" class="link" target="_blank">Pinformer</a> – Website listing pinball machines on location in the UK. Not always up to date.</p>
-
+<!-- ===== PAGE HERO ===== -->
+<div class="page-hero">
+    <p class="page-hero-eyebrow">UK Pinball League</p>
+    <h1 class="page-hero-title">Useful Links</h1>
 </div>
 
-<div class="panel">
+<!-- ===== MAIN CONTENT ===== -->
+<main class="site-content">
 
-<h2>Pinball clubs and museums</h2>
-<p><a href="http://www.flipoutlondon.com/" class="link" target="_blank">Flip Out London</a> – Pinball club based in Croydon. Open to the public for regular pinball. Also hosts a league and tournaments.</p>
-<p><a href="http://www.specialwhenlit.co.uk/index.php" class="link" target="_blank">Special When Lit</a> – Pinball club based in Salisbury. Open to the public for regular pinball. Also hosts a local league and annual tournament.</p>
-<p><a href="http://www.pinballparlour.co.uk/" class="link" target="_blank">The Pinball Parlour</a> – Pinball museum based in Ramsgate with machines from the 1960s to the 1990s available for play.</p>
+    <!-- Pinball Information -->
+    <div class="card">
+        <div class="card-header">
+            <div class="card-accent"></div>
+            <h2>Pinball Information</h2>
+        </div>
+        <ul class="link-list">
+            <li class="link-item">
+                <a href="https://en-gb.facebook.com/UKPinballLeague/" target="_blank" class="link-item-name">Facebook</a>
+                <span class="link-item-desc">The official facebook page for the UK Pinball League.</span>
+            </li>
+            <li class="link-item">
+                <a href="https://www.pinballinfo.com" target="_blank" class="link-item-name">Pinball Info</a>
+                <span class="link-item-desc">UK based forum for discussing all aspects of pinball.</span>
+            </li>
+            <li class="link-item">
+                <a href="https://www.pinballnews.com/site/" target="_blank" class="link-item-name">Pinball News</a>
+                <span class="link-item-desc">Reviews of latest machines, national and international tournament reports, location reports and more.</span>
+            </li>
+            <li class="link-item">
+                <a href="https://www.ifpapinball.com/" target="_blank" class="link-item-name">IFPA Pinball</a>
+                <span class="link-item-desc">World rankings of all registered pinball players.</span>
+            </li>
+            <li class="link-item">
+                <a href="https://www.ipdb.org/search.pl" target="_blank" class="link-item-name">Internet Pinball Database</a>
+                <span class="link-item-desc">Database of every pinball machine, including prototypes, production run data, pictures, flyers and manuals.</span>
+            </li>
+            <li class="link-item">
+                <a href="https://www.pinballnews.com/learn/acronyms.html" target="_blank" class="link-item-name">Acronyms</a>
+                <span class="link-item-desc">A list of all commonly used acronyms for machines; eg AFM is 'Attack from Mars'.</span>
+            </li>
+        </ul>
+    </div>
 
-</div>
+    <!-- Pinball Clubs and Museums -->
+    <div class="card">
+        <div class="card-header">
+            <div class="card-accent"></div>
+            <h2>Pinball Clubs and Museums</h2>
+        </div>
+        <ul class="link-list">
+            <li class="link-item">
+                <a href="https://www.pinballrepublic.com/" target="_blank" class="link-item-name">Pinball Republic London</a>
+                <span class="link-item-desc">UK's largest pinball club, based in Croydon. Open to the public. Also hosts a local league and tournaments.</span>
+            </li>
+            <li class="link-item">
+                <a href="https://specialwhenlit.co.uk/index.php" target="_blank" class="link-item-name">Special When Lit</a>
+                <span class="link-item-desc">Pinball club based in Salisbury. Open to the public for regular pinball. Also hosts a local league and tournaments.</span>
+            </li>
+            <li class="link-item">
+                <a href="https://pinballfactory.co.uk/" target="_blank" class="link-item-name">The Pinball Factory</a>
+                <span class="link-item-desc">Large modern pinball arcade based in Digbeth, Birmingham. Also hosts a local league and tournaments.</span>
+            </li>
 
-<div class="panel">
+            <li class="link-item">
+                <a href="https://www.pinballparlour.co.uk/" target="_blank" class="link-item-name">The Pinball Parlour</a>
+                <span class="link-item-desc">Pinball museum based in Margate with machines from the 1960s to the 1990s available for play.</span>
+            </li>
+        </ul>
+    </div>
 
-<h2>Other pinball leagues</h2>
-<p><a href="https://www.londonpinball.co.uk/" class="link" target="_blank">London Pinball League</a> – Local pinball league based in London.</p>
-<p><a href="http://www.specialwhenlit.co.uk/clubandleague.php" class="link" target="_blank">Special When Lit League</a> – Local pinball league based in Salisbury.</p>
+    <!-- Other Pinball Leagues -->
+    <div class="card">
+        <div class="card-header">
+            <div class="card-accent"></div>
+            <h2>Other Pinball Leagues</h2>
+        </div>
+        <ul class="link-list">
+            <li class="link-item">
+                <a href="https://www.londonpinball.co.uk/" target="_blank" class="link-item-name">London Pinball League</a>
+                <span class="link-item-desc">Local pinball league based in London.</span>
+            </li>
+            <li class="link-item">
+                <a href="https://www.specialwhenlit.co.uk/clubandleague.php" target="_blank" class="link-item-name">Special When Lit League</a>
+                <span class="link-item-desc">Local pinball league based in Salisbury.</span>
+            </li>
+        </ul>
+    </div>
 
-</div>
+    <!-- Machines, Parts and Repairs -->
+    <div class="card">
+        <div class="card-header">
+            <div class="card-accent"></div>
+            <h2>Pinball Machines, Parts and Repairs</h2>
+        </div>
+        <ul class="link-list">
+            <li class="link-item">
+                <a href="https://www.onestoppinball.co.uk/" target="_blank" class="link-item-name">1 Stop Pinball</a>
+                <span class="link-item-desc">Supplier of a range of pinball spares with a wide selection of LEDs.</span>
+            </li>
+            <li class="link-item">
+                <a href="https://www.homeleisuredirect.com/pinball-machines/" target="_blank" class="link-item-name">Home Leisure Direct</a>
+                <span class="link-item-desc">Seller of a wide variety of pinballs, pool tables and arcade machines.</span>
+            </li>
+            <li class="link-item">
+                <a href="https://www.libertygames.co.uk/store/pinball_machines/" target="_blank" class="link-item-name">Liberty Games</a>
+                <span class="link-item-desc">Games room specialists. New and refurbished machines for sale. On-site maintenance available.</span>
+            </li>
+            <li class="link-item">
+                <a href="https://www.londonpinball.co.uk/product-category/pinball-machines/" target="_blank" class="link-item-name">London Pinball</a>
+                <span class="link-item-desc">Buy and sell pinball machines. Always a large selection of refurbished machines for sale.</span>
+            </li>
+            <li class="link-item">
+                <a href="https://mayapinball.com/" target="_blank" class="link-item-name">MAYA Pinball</a>
+                <span class="link-item-desc">UK Manufacturer of pinball components and accessories.</span>
+            </li>
+            <li class="link-item">
+                <a href="https://www.pinball.co.uk/" target="_blank" class="link-item-name">Pinball Heaven</a>
+                <span class="link-item-desc">National pinball seller of new and refurbished machines. Also supplies parts.</span>
+            </li>
+            <li class="link-item">
+                <a href="https://www.pinball-led.co.uk/" target="_blank" class="link-item-name">Pinball LED</a>
+                <span class="link-item-desc">Seller of LEDs for pinball machines.</span>
+            </li>
+            <li class="link-item">
+                <a href="https://retroarcadespecialists.co.uk/" target="_blank" class="link-item-name">Retro Arcade Specialists</a>
+                <span class="link-item-desc">New and refurbished pinball machines, parts and repairs.</span>
+            </li>
+        </ul>
+    </div>
 
-<div class="panel">
-
-<h2>Pinball machines, parts and repairs</h2>
-<p><a href="https://www.onestoppinball.co.uk/" class="link" target="_blank">1 Stop Pinball</a> – Supplier of a range of pinball spares with a wide selection of LEDs.</p>
-<p><a href="https://www.homeleisuredirect.com/pinball-machines/" class="link" target="_blank">Home Leisure Direct</a> – Seller of a wide variety of pinballs, pool tables and arcade machines.</p>
-<p><a href="https://www.libertygames.co.uk/store/pinball_machines/" class="link" target="_blank">Liberty Games</a> – Games room specialists. New and refurbished machines for sale. On-site maintenance available.</p>
-<p><a href="https://www.londonpinball.co.uk/product-category/pinball-machines/" class="link" target="_blank">London Pinball</a> – Buy and sell pinball machines. Always a large selection of refurbished machines for sale.</p>
-<p><a href="https://davidwillcox.vpweb.co.uk/" class="link" target="_blank">Pinball Daze</a> – Pinball spares and servicing. Data East, Sega and Stern specialist. Other manufacturer's spares also available.</p>
-<p><a href="https://www.pinball.co.uk/" class="link" target="_blank">Pinball Heaven</a> – National pinball seller of new and refurbished machines. Also supplies parts.</p>
-<p><a href="https://www.pinball-led.co.uk/" class="link" target="_blank">Pinball LED</a> – Seller of LEDs for pinball machines.</p>
-<p><a href="https://www.pinballmadness.co.uk/" class="link" target="_blank">Pinball Madness</a> – Pinballs bought and sold. On-site repairs and servicing. Delivery service available.</p>
-<p><a href="https://pinparts.co.uk/" class="link" target="_blank">Pinball Mania</a> – Principal UK onsite repair service.  Also supplies parts and second hand machines.</p>
-
-</div>
+</main>
 
 <!-- Footer -->
-<?php include("includes/footer.inc"); ?>
+<?php require_once('includes/footer-modern.inc'); ?>
 
 </body>
 </html>
